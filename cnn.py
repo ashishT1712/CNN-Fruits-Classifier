c14dab38d2c4ae142a8bd3f5043fb7b425217703
Ashish Tiwari <31225925+ashishcoolstorm@users.noreply.github.com>
2018-06-19 13:46:18 -0400

    Add files via upload
    CNN model for fruits image classification. The model was built using Keras library for deep learning. The model was trained and the accuracy achieved was 93%. The model has three convolution layer in combination with max pooling layers.

---
+++ cnn.py
@@ -0,0 +1,69 @@
+# Convolutional Neural Network
+
+# Installing Theano
+# pip install --upgrade --no-deps git+git://github.com/Theano/Theano.git
+
+# Installing Tensorflow
+# pip install tensorflow
+
+# Installing Keras
+# pip install --upgrade keras
+
+# Part 1 - Building the CNN
+
+# Importing the Keras libraries and packages
+from keras.models import Sequential
+from keras.layers import Conv2D
+from keras.layers import MaxPooling2D
+from keras.layers import Flatten
+from keras.layers import Dense
+
+# Initialising the CNN
+classifier = Sequential()
+
+# Step 1 - Convolution
+classifier.add(Conv2D(32, (3, 3), input_shape = (100, 100, 3), activation = 'relu'))
+
+# Step 2 - Pooling
+classifier.add(MaxPooling2D(pool_size = (2, 2)))
+
+# Adding a second convolutional layer
+classifier.add(Conv2D(32, (3, 3), activation = 'relu'))
+classifier.add(MaxPooling2D(pool_size = (2, 2)))
+
+# Step 3 - Flattening
+classifier.add(Flatten())
+
+# Step 4 - Full connection
+classifier.add(Dense(units = 128, activation = 'relu'))
+classifier.add(Dense(units = 10, activation = 'sigmoid'))
+
+# Compiling the CNN
+classifier.compile(optimizer = 'adam', loss = 'categorical_crossentropy', metrics = ['accuracy'])
+
+# Part 2 - Fitting the CNN to the images
+
+from keras.preprocessing.image import ImageDataGenerator
+
+train_datagen = ImageDataGenerator(rescale = 1./255,
+                                   shear_range = 0.2,
+                                   zoom_range = 0.2,
+                                   horizontal_flip = True)
+
+test_datagen = ImageDataGenerator(rescale = 1./255)
+
+Training = train_datagen.flow_from_directory('Fruit-Images-Dataset/Training',
+                                                 target_size = (100, 100),
+                                                 batch_size = 128,
+                                                 class_mode = 'categorical')
+
+Validation = test_datagen.flow_from_directory('Fruit-Images-Dataset/Validation',
+                                            target_size = (100, 100),
+                                            batch_size = 128,
+                                            class_mode = 'categorical')
+
+classifier.fit_generator(Training,
+                         steps_per_epoch = 1000,
+                         epochs = 10,
+                         validation_data = Validation,
+                         validation_steps = 1000)
